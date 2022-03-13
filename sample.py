5d019630ddf09e915226c560554bc5c55076c88f
Dimitris Mangos <mitsmags@gmail.com>
2022-03-13 19:29:42 +0000

    samples

---
+++ sample.py
@@ -0,0 +1,20 @@
+from asyncio import streams
+import xml.etree.ElementTree as ET
+#get the XML file data
+stream=open('sample.xml','r')
+
+#parse the data into an ElementTree object
+xml= ET.parse(stream)
+
+#get the 'root' Element object from the ElementTree
+root=xml.getroot()
+
+#Iterate through each child of the root Element
+for e in root:
+    #print the stringified version of the element
+    print(ET.tostring(e))
+    print("")
+
+    #print the 'Id' attribute of the Element object
+    print(e.get("Id"))
+
